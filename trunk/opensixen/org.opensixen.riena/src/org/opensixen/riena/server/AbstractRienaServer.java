5b98ad8fc947ae589ad3032ec84dfe1fb7bb4a44
eloy_gomez <eloy_gomez@b9541ed4-effe-c974-70d7-6f2961cbac3c>
2011-07-26 16:47:09 +0000

    Refactor Abstract server classes

--- trunk/opensixen/org.opensixen.riena/src/org/opensixen/riena/server/AbstractRienaServer.java
+++ trunk/opensixen/org.opensixen.riena/src/org/opensixen/riena/server/AbstractRienaServer.java
@@ -59,21 +59,58 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-package org.opensixen.dev.omvc.server;
+package org.opensixen.riena.server;
 
+import java.util.Hashtable;
+
+import org.opensixen.riena.interfaces.IRienaService;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 /**
+ * Abstract Riena server
  * 
- * IRienaServer 
- *
+ * 
  * @author Eloy Gomez
  * Indeos Consultoria http://www.indeos.es
  *
  */
-public interface IRienaServer {
+public abstract class AbstractRienaServer implements IRienaServer, IRienaService {
 	
-	public void registerService(BundleContext context);
-	public void unregisterService(BundleContext context);
+	private static ServiceRegistration serviceRegistration; 
+		
+	private String serviceName;
+	
+	private String servicePath;
+	
+	
+	public AbstractRienaServer(String serviceName, String servicePath) {
+		super();
+		this.serviceName = serviceName;
+		this.servicePath = servicePath;
+	}
+
+	@Override
+	public boolean testService() {
+		return true;
+	}
+
+	@Override
+	public void registerService(BundleContext context) {
+		Hashtable<String, String> properties = new Hashtable<String, String>();
+		properties.put("riena.remote", "true");
+        properties.put("riena.remote.protocol", "hessian");
+        properties.put("riena.remote.path", servicePath);
+        
+        serviceRegistration = context.registerService(serviceName, this, properties);
+	}
+
+	@Override
+	public void unregisterService(BundleContext context) {
+		if (serviceRegistration != null)	{
+			serviceRegistration.unregister();
+			serviceRegistration = null;
+		}	
+	}
 
 }
--- trunk/opensixen/org.opensixen.riena/src/org/opensixen/riena/server/IRienaServer.java
+++ trunk/opensixen/org.opensixen.riena/src/org/opensixen/riena/server/IRienaServer.java
@@ -59,62 +59,21 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-package org.opensixen.dev.omvc.server;
+package org.opensixen.riena.server;
 
-import java.util.Hashtable;
-
-import org.apache.log4j.Logger;
-
-import org.opensixen.riena.interfaces.IRienaService;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 
 /**
- * Abstract Riena server
  * 
- * 
+ * IRienaServer 
+ *
  * @author Eloy Gomez
  * Indeos Consultoria http://www.indeos.es
  *
  */
-public abstract class AbstractRienaServer implements IRienaServer, IRienaService {
-
-	protected Logger log = Logger.getLogger(getClass());
+public interface IRienaServer {
 	
-	private static ServiceRegistration serviceRegistration; 
-		
-	private String serviceName;
-	
-	private String servicePath;
-	
-	
-	public AbstractRienaServer(String serviceName, String servicePath) {
-		super();
-		this.serviceName = serviceName;
-		this.servicePath = servicePath;
-	}
-
-	@Override
-	public boolean testService() {
-		return true;
-	}
-
-	@Override
-	public void registerService(BundleContext context) {
-		Hashtable<String, String> properties = new Hashtable<String, String>();
-		properties.put("riena.remote", "true");
-        properties.put("riena.remote.protocol", "hessian");
-        properties.put("riena.remote.path", servicePath);
-        
-        serviceRegistration = context.registerService(serviceName, this, properties);
-	}
-
-	@Override
-	public void unregisterService(BundleContext context) {
-		if (serviceRegistration != null)	{
-			serviceRegistration.unregister();
-			serviceRegistration = null;
-		}	
-	}
+	public void registerService(BundleContext context);
+	public void unregisterService(BundleContext context);
 
 }
