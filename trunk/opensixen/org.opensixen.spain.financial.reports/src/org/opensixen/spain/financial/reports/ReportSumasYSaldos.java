76f0533656748e0b409e3fe96cf8d22105d789f3
gomez.eloy@gmail.com <gomez.eloy@gmail.com@b9541ed4-effe-c974-70d7-6f2961cbac3c>
2012-01-12 14:41:54 +0000

    Added parameters

--- trunk/opensixen/org.opensixen.spain.financial.reports/src/org/opensixen/spain/financial/reports/ReportSumasYSaldos.java
+++ trunk/opensixen/org.opensixen.spain.financial.reports/src/org/opensixen/spain/financial/reports/ReportSumasYSaldos.java
@@ -61,12 +61,14 @@
 package org.opensixen.spain.financial.reports;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Properties;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
 
+import org.compiere.process.ProcessInfoParameter;
 import org.compiere.util.Env;
 import org.opensixen.model.ColumnDefinition;
 import org.opensixen.osgi.interfaces.ICommand;
@@ -79,7 +81,17 @@ import org.opensixen.report.AbstractDynamicReport;
  * @author Eloy Gomez
  * Indeos Consultoria http://www.indeos.es
  */
-public class ReportSumasYSaldos extends AbstractDynamicReport implements ICommand {
+public class ReportSumasYSaldos extends AbstractDynamicReport {
+
+	
+	private Timestamp p_DateFrom;
+	private Timestamp p_DateTo;
+	private int p_C_BPartner_ID;
+	private int p_M_Product_ID;
+	private int p_C_ElementFrom_ID;
+	private int p_C_ElementTo_ID;
+	private ProcessInfoParameter[] parameters;
+
 
 	/**
 	 * @param ctx
@@ -99,7 +111,7 @@ public class ReportSumasYSaldos extends AbstractDynamicReport implements IComman
 	protected ColumnDefinition[] getColumns() {
 		ColumnDefinition[] cols = { new ColumnDefinition("value", "Cuenta", String.class, 80),
 								new ColumnDefinition("name", "", String.class, 240),
-								new ColumnDefinition("saldoinicial", "Inicial", BigDecimal.class, 90),
+								new ColumnDefinition("saldoinicial", "Inicial", BigDecimal.class, 90),								
 								new ColumnDefinition("debe", "Debe", BigDecimal.class, 90),
 								new ColumnDefinition("haber", "Haber", BigDecimal.class, 90),
 								new ColumnDefinition("saldo", "saldo", BigDecimal.class, 90)
@@ -123,7 +135,7 @@ public class ReportSumasYSaldos extends AbstractDynamicReport implements IComman
 	 */
 	@Override
 	protected JRDataSource getDataSource() {
-		SumasYSaldosJasperDataSource ds = new SumasYSaldosJasperDataSource(Env.getCtx());
+		SumasYSaldosJasperDataSource ds = new SumasYSaldosJasperDataSource(Env.getCtx(), parameters);
 		ds.loadData();
 		return ds;
 		
@@ -132,16 +144,18 @@ public class ReportSumasYSaldos extends AbstractDynamicReport implements IComman
 	/* (non-Javadoc)
 	 * @see org.opensixen.osgi.interfaces.ICommand#prepare()
 	 */
-	@Override
-	public void prepare() {
-		// TODO Auto-generated method stub
+		
+	public void prepare(ProcessInfoParameter[] para) {
+		
+		parameters = para;
 		
 	}
+
 
 	/* (non-Javadoc)
 	 * @see org.opensixen.osgi.interfaces.ICommand#doIt()
 	 */
-	@Override
+
 	public String doIt() throws Exception {
 		initReport();
 		viewReport();
